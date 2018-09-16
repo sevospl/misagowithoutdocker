35fcfae87e5276be2979c7f4a56d904cd1c3a668
rafalp <kontakt@rpiton.com>
2018-09-16 22:27:43 +0200

    Rename misagodocker dir to misago

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+FROM python:3.6-slim
+ENV PYTHONUNBUFFERED 1
+
+# Install dependencies in one single command/layer
+RUN apt-get update && \
+    apt-get install -y \
+      vim \
+      libffi-dev \
+      libssl-dev \
+      sqlite3 \
+      libjpeg-dev \
+      libopenjpeg-dev \
+      locales \
+      cron \
+      postgresql-client
+      gettext && \
+    apt-get clean
+
+# Install requirements file
+ADD requirements.txt /
+RUN pip install -r requirements.txt
+
+# Make current directory available as "Misago" within docker
+ADD . /misago
+WORKDIR /misago
+
+# Expose port 80 from Docker
+EXPOSE 80
+
+# Call entrypoint script to setup 
+CMD ["uwsgi" "--ini uwsgi.ini"]
---
+++ maintenance.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# This is the cron job that does the scheduled maintenace on the Misago.
+# It deletes old sessions, refreshes active posters list, prunes categories
+# set to delete old threads, keeps read tracking record small, disables
+# expired bans, etc. ect.
+#
+# Tasks defined in this file are ran serially, one after another, making it
+# simpler to keep things in order (eg. active posters list is populated after
+# specified categories are pruned) and their memory usage is in check (no more
+# than single maintenance task is running at single time)
+#
+# You are free to edit this file to add custom `manage.py` calls for your site.
+
+# Make sure database is available
+./wait_for_postgres.sh
+
+# Run maintenance commands
+python manage.py deletemarkedusers
+python manage.py prunecategories
+python manage.py buildactivepostersranking
+python manage.py clearattachments
+python manage.py clearreadtracker
+python manage.py clearsessions
+python manage.py clearsocial
+python manage.py invalidatebans
+python manage.py removeoldips
+python manage.py deleteinactiveusers
+python manage.py expireuserdatadownloads
+python manage.py prepareuserdatadownloads
